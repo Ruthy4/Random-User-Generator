664631946519cac326ac7a8f47a2783ca30d70b8
ify <unokaify@gmail.com>
2023-05-31 22:49:42 +0100

    assign data class values to null

--- app/src/main/java/com/example/randomusergenerator/data/remote/dto/UserGeneratorResponse.kt
+++ app/src/main/java/com/example/randomusergenerator/data/remote/dto/UserGeneratorResponse.kt
@@ -2,55 +2,55 @@ package com.example.randomusergenerator.data.remote.dto
 
 data class UserGeneratorResponse(
     val result: List<Result>? = emptyList(),
-    val info: Info? = Info()
+    val info: Info? = null
 )
 
 
 data class Result(
-    val gender: String? = "",
-    val name: Name? = Name(),
-    val email: String? = "",
-    val location: Location? = Location(),
-    val login: LoginDetails? = LoginDetails(),
-    val dob: DateOfBirth? = DateOfBirth(),
-    val picture: PictureData? = PictureData(),
+    val gender: String? = null,
+    val name: Name? = null,
+    val email: String? = null,
+    val location: Location? = null,
+    val login: LoginDetails? = null,
+    val dob: DateOfBirth? = null,
+    val picture: PictureData? = null,
 )
 
 data class Info(
-    val seed: String? = "",
+    val seed: String? = null,
     val results: Int? = 0,
     val page: Int? = 0,
-    val version: String? = "",
+    val version: String? = null,
 )
 
 data class Name(
-    val title: String? = "",
-    val first: String? = "",
-    val last: String? = "",
+    val title: String? = null,
+    val first: String? = null,
+    val last: String? = null,
 )
 
 data class Location(
-    val street: Street? = Street(),
-    val city: String? = "",
-    val state: String? = "",
-    val country: String? = ""
+    val street: Street? = null,
+    val city: String? = null,
+    val state: String? = null,
+    val country: String? = null
 )
 
 data class Street(
     val number: Int? = 0,
-    val name: String? = "",
+    val name: String? = null,
 )
 
 data class LoginDetails(
-    val username: String? = "",
-    val password: String? = "",
+    val username: String? = null,
+    val password: String? = null,
 )
 
 data class DateOfBirth(
     val age: Int? = 0,
-    val date: String? = "",
+    val date: String? = null,
 )
 
 data class PictureData(
-    val thumbnail: String? = ""
+    val thumbnail: String? = null
 )
