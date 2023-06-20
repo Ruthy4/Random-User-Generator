ab0e004ec24510090ab9e5adedf8dca8f0a5db56
Ifeyinwa Unoka <unokaify@gmail.com>
2023-06-20 07:26:00 +0100

    service class setup

--- app/src/main/java/com/example/randomusergenerator/MainActivity.kt
+++ app/src/main/java/com/example/randomusergenerator/MainActivity.kt
@@ -3,24 +3,15 @@ package com.example.randomusergenerator
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.hilt.navigation.compose.hiltViewModel
 import com.example.randomusergenerator.presentation.ui.theme.RandomUserGeneratorTheme
-import com.example.randomusergenerator.presentation.user.UserViewModel
-import com.example.randomusergenerator.utils.Resource
 import dagger.hilt.android.AndroidEntryPoint
-import timber.log.Timber
+
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -32,43 +23,9 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    UserScreen()
                 }
             }
         }
-    }
-}
-
-@Composable
-fun UserScreen(userViewModel: UserViewModel = hiltViewModel()) {
-    val userState by userViewModel.userResponse.collectAsState()
-
-    when (val resource = userState) {
-        is Resource.Loading -> {
-            // Show loading indicator
-            CircularProgressIndicator()
-        }
-        is Resource.Success -> {
-            val userData = resource.data
-            if (userData != null) {
-                // Display the user data
-//                UserList(userData.result)
-                Timber.d("All Users ${userData.result}")
-            } else {
-                // Show empty state
-                Text("No users available")
-            }
-        }
-        is Resource.Error -> {
-            // Show error message
-            Text("Error occurred: ${resource.error?.message}")
-        }
-        else -> Unit
-    }
-
-    // Trigger data fetching
-    LaunchedEffect(Unit) {
-        userViewModel.getAllUsers(results = 10)
     }
 }
 
@@ -76,6 +33,5 @@ fun UserScreen(userViewModel: UserViewModel = hiltViewModel()) {
 @Composable
 fun DefaultPreview() {
     RandomUserGeneratorTheme {
-
     }
 }
