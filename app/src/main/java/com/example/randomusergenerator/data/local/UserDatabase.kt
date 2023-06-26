0ff8f4cfd7988f1ee6214bd28cadbcc22836e8fb
Ifeyinwa Unoka <unokaify@gmail.com>
2023-06-26 15:31:10 +0100

    setup unit test requirements

--- app/src/main/java/com/example/randomusergenerator/data/local/UserDatabase.kt
+++ app/src/main/java/com/example/randomusergenerator/data/local/UserDatabase.kt
@@ -3,6 +3,7 @@ package com.example.randomusergenerator.data.local
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
+import com.example.randomusergenerator.data.local.dao.UserDao
 import com.example.randomusergenerator.data.remote.dto.User
 import com.example.randomusergenerator.utils.TypeConverter
 
--- app/src/test/java/com/example/randomusergenerator/util/BaseDaoTest.kt
+++ app/src/test/java/com/example/randomusergenerator/util/BaseDaoTest.kt
@@ -1,4 +1,43 @@
 package com.example.randomusergenerator.util
 
-class BaseDaoTest {
+import android.content.Context
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.room.Room
+import androidx.test.core.app.ApplicationProvider
+import com.example.randomusergenerator.data.local.UserDatabase
+import com.example.randomusergenerator.data.local.dao.UserDao
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.asExecutor
+import org.junit.After
+import org.junit.Before
+import org.junit.Rule
+import java.io.IOException
+
+abstract class BaseDaoTest {
+    @get:Rule
+    val instantExecutorRule = InstantTaskExecutorRule()
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    @get: Rule
+    val coroutinesRule = MainCoroutineRule()
+
+    private lateinit var db: UserDatabase
+    lateinit var dao: UserDao
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    @Before
+    fun createDb() {
+        val context = ApplicationProvider.getApplicationContext<Context>()
+        db = Room.inMemoryDatabaseBuilder(context, UserDatabase::class.java)
+            .setTransactionExecutor(coroutinesRule.testDispatcher.asExecutor())
+            .allowMainThreadQueries()
+            .build()
+        dao = db.userDao()
+    }
+
+    @After
+    @Throws(IOException::class)
+    fun closeDb() {
+        db.close()
+    }
 }
