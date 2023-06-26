72329089c55323b526c1ac5a25dd7f53289450ca
Ifeyinwa Unoka <unokaify@gmail.com>
2023-06-26 20:55:44 +0100

    fixed sample data class

--- app/src/test/java/com/example/randomusergenerator/util/SampleDataClass.kt
+++ app/src/test/java/com/example/randomusergenerator/util/SampleDataClass.kt
@@ -2,42 +2,32 @@ package com.example.randomusergenerator.util
 
 import com.example.randomusergenerator.data.remote.dto.*
 
-val sampleUserGeneratorResponse = UserResponse(
-    results = listOf(
-        User(
-            gender = "male",
-            name = Name(
-                title = "Mr",
-                first = "John",
-                last = "Doe"
-            ),
-            email = "johndoe@example.com",
-            location = Location(
-                street = Street(
-                    number = 123,
-                    name = "Main Street"
-                ),
-                city = "New York",
-                state = "NY",
-                country = "USA"
-            ),
-            login = LoginDetails(
-                username = "johndoe123",
-                password = "password123"
-            ),
-            dob = DateOfBirth(
-                age = 30,
-                date = "1992-05-20"
-            ),
-            picture = Picture(
-                thumbnail = "https://example.com/thumbnail.jpg"
-            )
-        )
+val sampleUserResponse = User(
+    gender = "male",
+    name = Name(
+        title = "Mr",
+        first = "John",
+        last = "Doe"
     ),
-    info = Info(
-        seed = "randomseed",
-        results = 1,
-        page = 1,
-        version = "1.0"
+    email = "johndoe@example.com",
+    location = Location(
+        street = Street(
+            number = 123,
+            name = "Main Street"
+        ),
+        city = "New York",
+        state = "NY",
+        country = "USA"
+    ),
+    login = LoginDetails(
+        username = "johndoe123",
+        password = "password123"
+    ),
+    dob = DateOfBirth(
+        age = 30,
+        date = "1992-05-20"
+    ),
+    picture = Picture(
+        thumbnail = "https://example.com/thumbnail.jpg"
     )
 )
