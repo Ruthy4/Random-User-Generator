42e8d569a2805f69c8aa294c83fae4c45a6599a1
Ifeyinwa Unoka <33810711+Ruthy4@users.noreply.github.com>
2023-07-04 20:42:25 +0100

    Setup user repository tests (#13)

---
+++ app/src/test/java/com/example/randomusergenerator/data/repository/UserRepositoryImplTest.kt
@@ -0,0 +1,98 @@
+package com.example.randomusergenerator.data.repository
+
+import com.example.randomusergenerator.data.local.dao.UserDao
+import com.example.randomusergenerator.data.remote.ApiService
+import com.example.randomusergenerator.data.remote.dto.UserResponse
+import com.example.randomusergenerator.util.sampleUser
+import com.example.randomusergenerator.util.sampleUserData
+import com.example.randomusergenerator.util.sampleUserResponse
+import com.example.randomusergenerator.utils.Resource
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runTest
+import okhttp3.ResponseBody.Companion.toResponseBody
+import org.junit.Assert.*
+import org.junit.Before
+import org.junit.Test
+import org.mockito.ArgumentCaptor
+import org.mockito.Mock
+import org.mockito.MockitoAnnotations
+import org.mockito.kotlin.*
+import retrofit2.Response
+
+@OptIn(ExperimentalCoroutinesApi::class)
+class UserRepositoryImplTest {
+
+    @Mock
+    lateinit var mockApiService: ApiService
+
+    @Mock
+    lateinit var mockUserDao: UserDao
+
+    private lateinit var serviceUnderTest: UserRepositoryImpl
+
+    private val numberOfUsers = 10
+
+    val captor: ArgumentCaptor<Int> = ArgumentCaptor.forClass(Int::class.java)
+
+    @Before
+    fun setUp() {
+        MockitoAnnotations.openMocks(this)
+        serviceUnderTest = UserRepositoryImpl(mockApiService, mockUserDao)
+    }
+
+    @Test
+    fun `when getUsers is called then return a successful list of users`() = runTest {
+        val expectedData = Resource.Success(listOf(sampleUserData))
+        val userList = listOf(sampleUser)
+        // GIVEN
+        whenever(mockUserDao.getUser()).thenReturn(userList)
+        whenever(mockApiService.getUsers(numberOfUsers))
+            .thenReturn(Response.success(sampleUserResponse))
+        whenever(mockUserDao.updateUser(any())).thenReturn(Unit)
+
+        // WHEN
+        serviceUnderTest.getAllUsers(numberOfUsers)
+
+        // THEN
+
+        val initialDbResult = mockUserDao.getUser()
+        assertTrue(initialDbResult.isNotEmpty())
+        assertEquals(1, initialDbResult.size)
+
+        verify(mockApiService).getUsers(numberOfUsers)
+        verify(mockApiService).getUsers(captor.capture())
+
+        val capturedParam = captor.value
+
+        mockUserDao.updateUser(userList)
+        val actualDbResult = mockUserDao.getUser()
+        assertTrue(actualDbResult.isNotEmpty())
+        assertEquals(expectedData.data?.first()?.email, actualDbResult.first().email)
+        assertEquals(numberOfUsers, capturedParam)
+    }
+
+    @Test
+    fun `when network call is made and has an error, then return appropriate error response`() =
+        runTest {
+            val errorText = "There is an error"
+            val errorResponse = Response.error<UserResponse>(
+                404,
+                errorText.toResponseBody(null)
+            )
+            val userList = listOf(sampleUser)
+
+            // GIVEN
+            whenever(mockUserDao.getUser()).thenReturn(userList)
+            whenever(mockApiService.getUsers(numberOfUsers))
+                .thenReturn(errorResponse)
+
+            // WHEN
+            val result = serviceUnderTest.getAllUsers(numberOfUsers)
+
+            // THEN
+            verify(mockApiService).getUsers(numberOfUsers)
+            verify(mockApiService).getUsers(captor.capture())
+
+            assertTrue(result is Resource.Error)
+        }
+}
